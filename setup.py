d8fffe1f072c179dee6ed2658ae71ff98c050622
Prashant Thapaliya <prashantthapaliya8989@gmail.com>
2022-01-16 00:45:29 +0545

    Final pip installation

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(name = 'ahp_calculator',
-        version='1.0.4',
-        description='calculate the ahp',
-        author='me',
+        version='0.0.8',
+        description='AHP is one of the extensively used Multi Criteria Decision Making (MCDM) tool for processing multiple important objectives and weighting the criteria. The AHP allows to assign a priority among various alternatives and integrating multidimensional measures into a single scale of priorities.',
+        author='Rohit Gautam, Prashant Thapaliya',
+        author_email= 'rowheat02@gmail.com',
+        long_description = long_description,
+        long_description_content_type="text/markdown",
+        url='https://github.com/Geospasolutions/AHP_Calculator/',
         packages=['ahp_calculator'],
-        zip_safe = False)
+        license='LICENSE',
+        install_requires=[
+            'ipywidgets==7.6.5',
+            'numpy==1.20.3'],
+        zip_safe = False,)
