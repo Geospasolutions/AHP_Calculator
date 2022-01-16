857d1bf7102a927967f82a3d6eca10e1aa1749df
Prashant Thapaliya <prashantthapaliya8989@gmail.com>
2022-01-16 17:12:26 +0545

    Beta version

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(name = 'ahp_calculator',
-        version='0.0.8',
+        version='0.1.0',
         description='AHP is one of the extensively used Multi Criteria Decision Making (MCDM) tool for processing multiple important objectives and weighting the criteria. The AHP allows to assign a priority among various alternatives and integrating multidimensional measures into a single scale of priorities.',
-        author='Rohit Gautam, Prashant Thapaliya',
+        author='Rohit Gautam, Prashant Thapaliya, Binabh Devkota',
         author_email= 'rowheat02@gmail.com',
         long_description = long_description,
         long_description_content_type="text/markdown",
